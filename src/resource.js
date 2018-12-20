fdff36343aa46240e4fa41b03eb8561de5161ddc
Bartłomiej Lechowski <frontkick.it@gmail.com>
2018-12-20 09:23:11 +0100

    AB-35 convertMongooseObject method

--- src/resource.js
+++ src/resource.js
@@ -69,12 +69,12 @@ class Resource extends BaseResource {
       .skip(offset)
       .limit(limit)
       .sort(sortingParam)
-    return mongooseObjects.map(mongooseObject => new BaseRecord(mongooseObject.toObject(), this))
+    return mongooseObjects.map(mongooseObject => new BaseRecord(this.convertMongooseObject(mongooseObject), this))
   }
 
   async findOne(id) {
-    const mongooseObject = await this.MongooseModel.findById(id)
-    return new BaseRecord(mongooseObject.toObject(), this)
+    let mongooseObject = await this.MongooseModel.findById(id)
+    return new BaseRecord(this.convertMongooseObject(mongooseObject), this)
   }
 
   build(params) {
@@ -123,6 +123,15 @@ class Resource extends BaseResource {
       return memo
     }, {})
     return new ValidationError(`${this.name()} validation failed`, errors)
+  }
+
+  convertMongooseObject(mongooseObj) {
+    const obj = mongooseObj.toObject()
+
+    // By default the _id field is an ObjectID, one of MongoDB's BSON
+    // We have to convert this field to string for the flatten record params
+    obj._id = obj._id.toString()
+    return obj
   }
 }
 
